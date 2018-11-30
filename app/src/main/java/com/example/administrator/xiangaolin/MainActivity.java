7e942943c95dc0a676e67fa5a1ebf4f23f41b6f1
xiangaolin <1832707139@qq.com>
2018-11-30 16:01:37 +0800

    第一次变更

--- app/src/main/java/com/example/administrator/xiangaolin/MainActivity.java
+++ app/src/main/java/com/example/administrator/xiangaolin/MainActivity.java
@@ -9,7 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+       initview();
 
+    }
+
+    private void initview() {
+        String name="huhu";
+        int a=1;
     }
 }
